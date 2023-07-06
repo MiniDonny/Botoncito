32460bf03f0d00054601abe14abb401c6cc73aca
Donato Robledo <129457178+MiniDonny@users.noreply.github.com>
2023-07-06 17:38:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function touch(){
+ /*function touch(){
     alert("TOUCH ME")
-
     document.querySelector(".botontoutch").classList.toggle(".touchme")
+}*/
 
+function generarPapucho(){
+    document.querySelector(".contenedor").innerHTML += '<div class="meme"></div>'
 }
